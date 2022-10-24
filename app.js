cb82e8ac1fc654c441fd703691e7b42f25b4000d
Minh Nguyen <lambandwoof@gmail.com>
2022-10-24 14:27:41 -0500

    third commit

--- app.js
+++ app.js
@@ -1,29 +1,71 @@
 const express = require("express");
-const morgan = require('morgan')
-const postBank = require('./postBank')
+const morgan = require("morgan");
+const postBank = require("./postBank");
 const app = express();
-app.use(express.static('public'))
 
-app.use(morgan('dev'));
-
-
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
-  const html = `<!DOCTYPE html>
+  const html = 
+  `<!DOCTYPE html>
   <html>
   <head>
     <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
   </head>
   <body>
-  <ul>
-  ${posts.map(post => `<li>${posts}</li>`)}
-  </ul>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
   </body>
-  </html>`;
-  res.send(html)
+</html>`;
+  res.send(html);
 });
 
+app.get('/posts/:id', (req, res) =>{
+  const id = req.params.id;
+  const post = postBank.find(id)
+  const posts = postBank.list()
+  const html = 
+  `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>
+  `
+  res.send(html);
+})
 
 const PORT = 1337;
 
